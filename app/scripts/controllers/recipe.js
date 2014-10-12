b62d51159b18aff9bbebf51db35c94c03332e325
Nicolas Santini <nicolas.santini@findly.com>
2014-10-13 07:42:24 +1300

    adding ingredient ids to recipe

--- app/scripts/controllers/recipe.js
+++ app/scripts/controllers/recipe.js
@@ -1,12 +1,23 @@
 'use strict';
 
-app.controller('RecipeCtrl', function($scope, $location, $routeParams, Recipe) {
+app.controller('RecipeCtrl', function($scope, $location, $routeParams, Recipe, Ingredient) {
 
   $scope.recipe = Recipe.find($routeParams.recipeId);
+
+  //TODO this needs to be filtered and loaded on demand!
+  $scope.ingredients = Ingredient.all;
 
   $scope.submitRecipe = function() {
     Recipe.create($scope.recipe).then(function(recipeId) {
       $location.path('/recipes/' + recipeId);
     });
   };
+
+  $scope.ingredientSelected = function() {
+    if (!$scope.recipe.ingredients) {
+      $scope.recipe.ingredients = [];
+    }
+    $scope.recipe.ingredients.push($scope.ingredient);
+    $scope.ingredient = "";
+  }
 });
--- app/scripts/services/recipe.js
+++ app/scripts/services/recipe.js
@@ -34,6 +34,9 @@ app.factory('Recipe', function($firebase, FIREBASE_URL, User) {
           });
         }
       }
+    },
+    ingredients: function (username) {
+      return $firebase(new Firebase(FIREBASE_URL + 'recipe_ingredients/' + username));
     }
   };
 
