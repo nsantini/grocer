3f3265200c7f4422d5c57e4551ed60b57a9ee3d5
Nicolas Santini <nicolas.santini@findly.com>
2014-10-23 09:01:35 +1300

    refactor grocery list controller and service

--- app/scripts/controllers/grocerylist.js
+++ app/scripts/controllers/grocerylist.js
@@ -1,31 +1,13 @@
 'use strict';
 
-app.controller('GrocerylistCtrl', function($scope, User, Schedule, Day, Recipe) {
+app.controller('GrocerylistCtrl', function($scope, User, GroceryList) {
   $scope.ingredients = {};
 
-  function loadGroceryList() {
-    var user = User.getCurrent();
-    Schedule.scheduleDaysAsArray(user.$id).$loaded().then(function(days) {
-      for (var d = 0; d < days.length; d++) {
-        Day.recipesAsArray(days.$getRecord(days.$keyAt(d)).$id).$loaded().then(function(recipes) {
-          for (var r = 0; r < recipes.length; r++) {
-            Recipe.ingredientsAsArray(recipes.$getRecord(recipes.$keyAt(r)).$id).$loaded().then(function(list) {
-              for (var i = 0; i < list.length; i++) {
-                var item = list.$getRecord(list.$keyAt(i)).$value;
-                $scope.ingredients[item] = ($scope.ingredients[item] ? $scope.ingredients[item] + 1 : 1);
-              }
-            });
-          }
-        });
-      }
-    });
-  };
-
   if (User.signedIn()) {
-    loadGroceryList();
+    GroceryList.loadGroceryList($scope.ingredients);
   }
 
   $scope.$on('CurrentUserSet', function () {
-    loadGroceryList();
+    GroceryList.loadGroceryList($scope.ingredients);
   });
 });
